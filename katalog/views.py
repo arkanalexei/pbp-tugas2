58fc6e33b09a519185d3ef8d9edcec7f0d0e4cae
arkanalexei <arkanalexei@gmail.com>
2022-09-11 23:42:31 +0700

    should work

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_katalog_item = CatalogItem.objects.all()
+context = {
+    "list_katalog_item": data_katalog_item,
+    "nama": "Arkan"
+}
