14b5b5f652892d282df4f6f9e064dabc4a83abd7
arkanalexei <arkanalexei@gmail.com>
2022-09-12 20:23:16 +0700

    added style.css and test

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+class UnitTest(TestCase):
+
+    def test_katalog_is_exist(self):
+        response  = Client().get('/katalog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
