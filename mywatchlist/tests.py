6884a09beb0aa3c5584b864eed4417d01f5d015e
arkanalexei <arkanalexei@gmail.com>
2022-09-19 19:59:06 +0700

    added tests

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -7,6 +7,18 @@ class UnitTest(TestCase):
     def test_mywatchlist_is_exist(self):
         response  = Client().get('/mywatchlist/')
         self.assertEqual(response.status_code, 200)
+    
+    def test_mywatchlist_html_is_exist(self):
+        response  = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_mywatchlist_json_is_exist(self):
+        response  = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_mywatchlist_xml_is_exist(self):
+        response  = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
         response = self.client.get('/mywatchlist/')
