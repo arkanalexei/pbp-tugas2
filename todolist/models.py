0912270a2d4d97c179d86fcdd49a6c9a2744a53a
arkanalexei <arkanalexei@gmail.com>
2022-09-23 13:09:12 +0700

    tugas 4

---
+++ todolist/models.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import settings
+
+# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    date = models.DateField(default=timezone.now)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
---
+++ todolist/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from todolist.models import Task
+from todolist.forms import TaskForm
+
+# Create your views here.
+@login_required(login_url='/todolist/login/')
+def show_todolist(request):
+    tasks = Task.objects.filter(user=request.user)
+    context = {
+        'username': request.COOKIES['username'],
+        'last_login': request.COOKIES['last_login'],
+        'tasks': tasks,
+    }
+    return render(request, "todolist.html", context)
+
+
+@login_required(login_url='/todolist/login/')
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            todolist = form.save(commit=False)
+            todolist.user = request.user
+            todolist.save()
+            return redirect('todolist:show_todolist')
+
+    
+    form = TaskForm()
+
+    context = {"forms": form,}
+
+    return render(request, 'create_task.html', context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('todolist:login')
+    
+    context = {'form':form, 'user':request.user}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("todolist:show_todolist")) # membuat response
+            response.set_cookie('username', username)
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('todolist:login'))
+    response.delete_cookie('last_login')
+    return redirect('todolist:login')
